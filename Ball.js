c1ff47ce15a48d6257645c325d1099b091cb971b
allen <zhenga@ufl.edu>
2022-09-10 15:07:16 -0400

    update

--- Ball.js
+++ Ball.js
@@ -2,4 +2,16 @@ export default class Ball {
     constructor(ballElm) {
         this.ballElm = ballElm
     }
+
+get x() {
+  return getComputedStyle(this.ballElm).getPropertyValue("--x");
+}
+
+set x(value) {
+    this.ballElm.style.setProperty("--x", value);
+}
+
+    update(delta) {
+        this.x = 5;
+    }
 }
---
+++ index.js
@@ -0,0 +1,17 @@
+import Ball from "./Ball";
+
+const ball = new Ball(document.getElementById("ball"));
+
+let lastTime;
+
+function update(time) {
+if (lastTime != null) {
+    const delta = time- lastTime;
+    ball.update(delta);
+}
+
+    lastTime = time;
+    window.requestAnimationFrame(update);
+}
+
+window.requestAnimationFrame(update);
